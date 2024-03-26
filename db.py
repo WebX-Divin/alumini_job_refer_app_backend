7ef6c45782bc712927adf7b2957b56fdcf641444
Divin Dass <116546151+WebX-Divin@users.noreply.github.com>
2024-03-26 10:35:24 +0530

    DB Update

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 # MongoDB connection
-MONGO_URI = "mongodb+srv://webx-divin:pass@alumini-job-refer-app.uj5baq2.mongodb.net/alumni_job_refer_app"
+MONGO_URI = "mongodb+srv://webx-divin:pass@alumini-job-refer-app.uj5baq2.mongodb.net/"
 client = MongoClient(MONGO_URI)
 db = client["alumni_job_refer_app"]
 users_collection = db["users"]
--- schemas/model.py
+++ schemas/model.py
@@ -1,4 +1,12 @@
 from pydantic import BaseModel, EmailStr, Field
+from typing import Union
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Union[str, None] = None
 
 # Pydantic model for request body
 class SignupRequest(BaseModel):
